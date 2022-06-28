d0f3315e20754681807491af498f8be52cc74c2e
Rayenire <aeristarg@gmail.com>
2022-06-28 13:33:53 +0300

    add task03

---
+++ task03/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+void n1 (int n) {
+  if (n == 6 || n == 7) {
+  Console.WriteLine("this is weekend");
+  }
+  else if (n < 1 || n > 7) {
+    Console.WriteLine("this is not the day of the week at all");
+  }
+  else Console.WriteLine("this isn't weekend");
+}
+
+n1(n);
+
