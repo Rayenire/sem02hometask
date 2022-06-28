5ed42d680cfb00f706b4144a4ea92b0ffda710e9
Rayenire <aeristarg@gmail.com>
2022-06-28 13:25:51 +0300

    add task02

---
+++ task02/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+string n1 = Convert.ToString(n);
+if (n1.Length > 2){
+  Console.WriteLine("Third number - " + n1[2]);
+}
+else {
+  Console.WriteLine("There's no third number");
+}
